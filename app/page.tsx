ca002001a97c63b5c4b9334abc47011ac06608e2
craigdenlinger <craig.denlinger@gmail.com>
2026-03-16 16:45:30 -0600

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { LeaderboardTable } from '@/components/LeaderboardTable';
 import { getPublicMetadata } from '@/lib/data';
 
+export const dynamic = 'force-dynamic';
+
 export default async function HomePage() {
   const meta = await getPublicMetadata();
 
