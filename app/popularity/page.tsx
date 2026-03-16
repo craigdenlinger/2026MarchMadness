b5f91fae9a581d0c9868a5971df1f23fa390eee6
craigdenlinger <craig.denlinger@gmail.com>
2026-03-16 16:45:51 -0600

    Update page.tsx

--- app/popularity/page.tsx
+++ app/popularity/page.tsx
@@ -2,6 +2,8 @@ import Link from 'next/link';
 import { TeamPopularityChart } from '@/components/TeamPopularityChart';
 import { getPublicMetadata } from '@/lib/data';
 
+export const dynamic = 'force-dynamic';
+
 export default async function PopularityPage() {
   const meta = await getPublicMetadata();
 
